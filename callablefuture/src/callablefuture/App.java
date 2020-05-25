cfd775fb7a1d46e948aa974278469a064c8e2bd4
Gajakosh <suhas.gajakosh@capgemini.com>
2020-05-25 15:51:51 +0530

    first commit

---
+++ callablefuture/src/callablefuture/App.java
@@ -0,0 +1,45 @@
+package callablefuture;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ExecutorService executor = Executors.newCachedThreadPool();
+
+		Future<Integer> future = executor.submit(new Callable<Integer>() {
+
+			@Override
+			public Integer call() throws Exception {
+				Random random = new Random();
+				int duration = random.nextInt(4000);
+
+				System.out.println("Starting...");
+				Thread.sleep(duration);
+
+				System.out.println("Completed in duration - " + duration);
+
+				return duration;
+			}
+		});
+		executor.shutdown();
+		
+		try {
+			System.out.println("Execution duration is - " + future.get());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ multiplelocks/src/multiplelocks/App.java
@@ -0,0 +1,12 @@
+package multiplelocks;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		//Worker worker = new Worker();
+		Worker2 worker = new Worker2();
+		worker.main();
+	}
+
+}
